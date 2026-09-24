95f8a3d563701e5f06c0dfbdcba6a8136fe48513
oisee <oisee@users.noreply.github.com>
2026-09-24 02:01:05 +0000

    Deploy pr-51 from bba1eb3fac5339603b698532cb457b2e246fd31b bba1eb3fac5339603b698532cb457b2e246fd31b

---
+++ pr-51/app/taxi/Component.js
@@ -0,0 +1,6 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+  "use strict";
+  return AppComponent.extend("stg.taxi.Component", {
+    metadata: {manifest: "json"},
+  });
+});
